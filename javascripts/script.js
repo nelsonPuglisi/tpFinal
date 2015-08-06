ffb1e64d3713a4d3ebbeee18869f8f4599fb1423
Nelsito <nelson.puglisi@gmail.com>
2015-08-06 20:37:17 -0300

    contenido

---
+++ javascripts/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows:false,
+		dots: false
+	});
+
+	$('.nota').on("mouseenter", function() {
+				$(this).css("opacity", "0");
+				$(this).parent().find("img").css("opacity", '1');
+
+
+		});
+
+	$('.nota').on("mouseleave", function() {
+				$(this).css("opacity", "1");
+				$(this).parent().find("img").css("opacity", '0.5');
+
+		});
+
+	$.get("datos.json", function(datos){
+		$.each(datos.notas, function(nota){
+		var div = '<div id="box" class="col-xs-4"><img class="imagenes" src="' + nota.imagen + '" width="100%"/><a class="nota" href="#"><h2 id="texto">'+nota.titulo+'</h2></a></div>';	
+		$("#notasContainer").append(div);
+		});
+	});
+});
